1a15ba46886c8580affe90b742963d472a175392
hitorilabs <131238467+hitorilabs@users.noreply.github.com>
2023-12-25 13:11:13 +0000

    remove some junk

--- servers/fastapi-server/main.py
+++ servers/fastapi-server/main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI, Request, Response
-from models import Event, CharmCompany
 
 app = FastAPI()
 
 
-@app.post("/")
-async def root(event: Event):
-    print(event)
-    if event.type == 1: return {"type": 1}
-    return {"type": -1}
-
-@app.post("/company")
-async def root(company: CharmCompany):
-    return company
+@app.get("/")
+async def root():
+    return {"message": "hello"}
